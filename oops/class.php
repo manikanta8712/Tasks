b49e5c295e477da4ae21b6c5decd0ac5c27f0ad5
manikanta8712 <manikanta@igreensystems.com>
2023-07-27 17:50:12 +0530

    oops added

--- oops/class.php
+++ oops/class.php
@@ -5,21 +5,68 @@
 
 
     <?php
-    class Fruit
+    class User
     {
-        public $name;
-        public $color;
-
-        function set_name($name)
+        protected $email;
+        private $name;
+        public $role = "member";
+        public function __construct($email, $name)
         {
+            //    echo $this->email = $email;
+            //   echo  $this->name = $name;
+            $this->email = $email;
             $this->name = $name;
         }
-
-        function get_name()
+        public function login($msg)
         {
-            return $this->name;
+            // echo "you logged in successfully";
+            echo $msg;
+        }
+        public function message()
+        {
+             return "$this->email you logged in successfully";
+        }
+        // getters
+        public function getUser()
+        {
+            return  $this->email;
+        }
+        public function setUser($email)
+        {
+            return $this->email = $email;
         }
     }
+    class AdminUser extends User
+    {
+        public $level;
+        public $role = "admin";
+        public function __construct($email, $name, $level)
+        {
+            $this->level = $level;
+            parent::__construct($email, $name);
+        }
+        public function message()
+        {
+             return "$this->email, an admin,sent new message";
+        }
+    }
+     $loginOne = new User("manikanta@gmail.com", "manikanta");
+    // $loginOne->login("you logged in successfully");
+    // echo $loginOne->name;
+    // echo $loginOne->email."</br>";
+
+    // $loginTwo = new User("king@gmail.com", "king");
+    // echo $loginTwo->name;
+    // echo $loginTwo->email."</br>";
+    // echo  $loginOne->getUser();
+    // echo  $loginOne->setUser("manikanta@gmail.com");
+    $loginThree = new AdminUser("virat@gmail.com", "virat", 5);
+    echo $loginThree->getUser();
+    echo $loginThree->level;
+    echo $loginOne->role."</br>";
+    echo $loginThree->role;
+    echo $loginOne->message()."</br>";
+    echo $loginThree->message()."</br>";
     ?>
 </body>
 
--- oops/object.php
+++ oops/object.php
@@ -2,31 +2,30 @@
 
 <body>
     <h1>class</h1>
-
-
     <?php
-    class Fruit
-    {
-        public $name;
-        public $color;
+   class Weather{
+    public static $tempcondition = ['cold','mild','warm'];
 
-        function set_name($name)
-        {
-            $this->name = $name;
+    public static function celsisToFarenheit($c){
+        return $c*9/5+32;
+    }
+    public static function determineTempCondition($f){
+        if($f < 40){
+            return self::$tempcondition[0];
         }
-
-        function get_name()
-        {
-            return $this->name;
+       else if($f < 70){
+            return self::$tempcondition[1];
+        }
+        else{
+            return self::$tempcondition[2];
         }
     }
-    $apple = new Fruit();
-    $banana = new Fruit();
-    $apple->set_name('Apple');
-    $banana->set_name('Banana');
-   echo $apple->get_name();
-   echo "</br>";
-   echo $banana->get_name();
+   }
+   //$condition = new Weather();
+   //print_r($condition);
+   print_r(Weather::$tempcondition);
+   echo Weather::celsisToFarenheit(20);
+   echo Weather::determineTempCondition(20);
     ?>
 </body>
 
